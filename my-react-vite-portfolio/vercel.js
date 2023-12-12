6ed87f0f76e2c0838b551e19c0d872cf8105639c
Tolitz1 <116473735+Tolitz1@users.noreply.github.com>
2023-12-12 15:39:35 +0800

    4043

--- my-react-vite-portfolio/vercel.js
+++
@@ -1,8 +0,0 @@
-{
-    "routes": [
-        { "handle": "filesystem" },
-        { "src": "/Project", "dest": "/Project" },
-        { "src": "/Contact", "dest": "/Contact" },
-        { "src": "/", "dest": "/Dashboard" }
-    ]
-}
